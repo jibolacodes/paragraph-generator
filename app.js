ba419ae2c485b079b8cb867f01482697fa81dfef
Ajibola Akindunbi <jibzsupreme@gmail.com>
2021-10-15 16:33:43 +0100

    app

---
+++ app.js
@@ -0,0 +1,23 @@
+// random texts
+
+const text = [
+    `Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+    modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
+    totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
+    quasi aliquam eligendi, placeat qui corporis!`,
+    `Quaerat provident commodi consectetur veniam similique ad 
+    earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
+    fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
+    suscipit quas?`,
+    `temporibus enim commodi iusto libero magni deleniti quod quam 
+    consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+    doloremque`,
+    `minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+    quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
+    fugiat`,
+    `Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+    sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+    recusandae alias error harum maxime adipisci amet laborum.`
+];
+
+// selecting target variables
