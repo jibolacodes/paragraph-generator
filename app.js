d102958380f76ab8ba0a9ff365ce24582b68e807
Ajibola Akindunbi <jibzsupreme@gmail.com>
2021-10-16 13:01:08 +0100

    app

--- app.js
+++ app.js
@@ -2,22 +2,44 @@
 
 const text = [
     `Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
-    modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
-    totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
-    quasi aliquam eligendi, placeat qui corporis!`,
+    modi minima sunt esse `,
     `Quaerat provident commodi consectetur veniam similique ad 
-    earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
-    fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
-    suscipit quas?`,
+    earum omnis `,
     `temporibus enim commodi iusto libero magni deleniti quod quam 
-    consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-    doloremque`,
+    consequuntur! `,
     `minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
-    quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
-    fugiat`,
+    quibusdam `,
     `Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-    sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-    recusandae alias error harum maxime adipisci amet laborum.`
+    sapiente officiis `,
+    `Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+    modi minima sunt esse `,
+    `Quaerat provident commodi consectetur veniam similique ad 
+    earum omnis `,
+    `temporibus enim commodi iusto libero magni deleniti quod quam 
+    consequuntur! `,
+    `minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+    quibusdam `
 ];
 
 // selecting target variables
+const form = document.querySelector('.generate');
+const number = document.getElementById('number');
+const output = document.querySelector('.output');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const value = parseInt(number.value);
+    const randomNo = Math.floor(Math.random() * text.length);
+
+    console.log(randomNo);
+    if(isNaN(value) || value < 0 || value > 9 ){
+        output.innerHTML = `<p class='para'>${text[randomNo]}</p>`;
+    } else {
+        let tempText = text.slice(0, value);
+        tempText = tempText.map(function(item){
+            return `<p class='para'>${item}</p>`;
+        }).join('')
+        output.innerHTML = tempText;
+    }
+})
